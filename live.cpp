0c045cee75fcf052978d597da1250253f0c9b5e0
Paul Lesur <git@lesurpaul.fr>
2020-04-02 20:28:12 +0200

    wip it works for now :<

--- live.cpp
+++ live.cpp
@@ -28,20 +28,18 @@ struct Detector
     int demo_classes;
 
     network *net;
-    image buff[3];
-    image buff_letter[3];
-    int buff_index = 0;
+    image buff;
+    image buff_letter;
     void *cap;
     float fps = 0;
     float demo_thresh = 0;
     float demo_hier = .5;
     int running = 0;
 
-    int demo_frame = 3;
-    int demo_index = 0;
+    int avg_frame = 3;
+    int avg_index = 0;
     float **predictions;
     float *avg;
-    int demo_done = 0;
     int demo_total = 0;
     double demo_time;
 
@@ -51,8 +49,6 @@ struct Detector
     void remember_network(network *net);
     detection *avg_predictions(network *net, int *nboxes);
 
-    void yolo_really();
-
     Detector(char *cfgfile,
              char *weightfile,
              float thresh,
@@ -60,7 +56,7 @@ struct Detector
              const char *filename,
              char **names,
              int classes,
-             int delay,
+             int /* delay */,
              char *prefix,
              int avg_frames,
              float hier,
@@ -69,7 +65,7 @@ struct Detector
              int frames,
              int fullscreen)
     {
-        // demo_frame = avg_frames;
+        avg_frame = avg_frames;
         image **alphabet = load_alphabet();
         demo_names = names;
         demo_alphabet = alphabet;
@@ -80,12 +76,11 @@ struct Detector
         net = load_network(cfgfile, weightfile, 0);
         set_batch_network(net, 1);
 
-        srand(2222222);
-
         int i;
+
         demo_total = size_network(net);
-        predictions = reinterpret_cast<float **>(calloc(demo_frame, sizeof(float *)));
-        for (i = 0; i < demo_frame; ++i)
+        predictions = reinterpret_cast<float **>(calloc(avg_frame, sizeof(float *)));
+        for (i = 0; i < avg_frame; ++i)
         {
             predictions[i] = reinterpret_cast<float *>(calloc(demo_total, sizeof(float)));
         }
@@ -104,32 +99,24 @@ struct Detector
         if (!cap)
             error("Couldn't connect to webcam.\n");
 
-        buff[0] = get_image_from_stream(cap);
-        buff[1] = copy_image(buff[0]);
-        buff[2] = copy_image(buff[0]);
-        buff_letter[0] = letterbox_image(buff[0], net->w, net->h);
-        buff_letter[1] = letterbox_image(buff[0], net->w, net->h);
-        buff_letter[2] = letterbox_image(buff[0], net->w, net->h);
+        buff = get_image_from_stream(cap);
+        buff_letter = letterbox_image(buff, net->w, net->h);
 
         if (!prefix)
         {
-            make_window("Demo", 1352, 1013, fullscreen);
+            make_window((char *)"Demo", 1352, 1013, fullscreen);
         }
-
-        demo_time = what_time_is_it_now();
     }
 
     void run_once()
     {
         int count = 0;
-        while (!demo_done)
-        {
-            buff_index = (buff_index + 1) % 3;
             fps = 1. / (what_time_is_it_now() - demo_time);
             demo_time = what_time_is_it_now();
-            yolo_really();
+            fetch_in_thread();
+            detect_in_thread();
+            display_in_thread();
             ++count;
-        }
     }
 };
 
@@ -142,7 +129,7 @@ void Detector::remember_network(network *net)
         layer l = net->layers[i];
         if (l.type == YOLO || l.type == REGION || l.type == DETECTION)
         {
-            memcpy(predictions[demo_index] + count,
+            memcpy(predictions[avg_index] + count,
                    net->layers[i].output,
                    sizeof(float) * l.outputs);
             count += l.outputs;
@@ -155,9 +142,9 @@ detection *Detector::avg_predictions(network *net, int *nboxes)
     int i, j;
     int count = 0;
     fill_cpu(demo_total, 0, avg, 1);
-    for (j = 0; j < demo_frame; ++j)
+    for (j = 0; j < avg_frame; ++j)
     {
-        axpy_cpu(demo_total, 1. / demo_frame, predictions[j], 1, avg, 1);
+        axpy_cpu(demo_total, 1. / avg_frame, predictions[j], 1, avg, 1);
     }
     for (i = 0; i < net->n; ++i)
     {
@@ -169,28 +156,16 @@ detection *Detector::avg_predictions(network *net, int *nboxes)
         }
     }
     detection *dets = get_network_boxes(
-        net, buff[0].w, buff[0].h, demo_thresh, demo_hier, 0, 1, nboxes);
+        net, buff.w, buff.h, demo_thresh, demo_hier, 0, 1, nboxes);
     return dets;
-}
-
-void Detector::yolo_really()
-{
-    fetch_in_thread();
-    detect_in_thread();
-    display_in_thread();
 }
 
 void Detector::display_in_thread()
 {
-    int c = show_image(buff[(buff_index + 1) % 3], "Demo", 1);
+    int c = show_image(buff, "Demo", 1);
     if (c != -1)
         c = c % 256;
-    if (c == 27)
-    {
-        demo_done = 1;
-        return;
-    }
-    else if (c == 82)
+    if (c == 82)
     {
         demo_thresh += .02;
     }
@@ -222,12 +197,6 @@ static float get_pixel_extend(image m, int x, int y, int c)
 {
     if (x < 0 || x >= m.w || y < 0 || y >= m.h)
         return 0;
-    /*
-    if(x < 0) x = 0;
-    if(x >= m.w) x = m.w-1;
-    if(y < 0) y = 0;
-    if(y >= m.h) y = m.h-1;
-    */
     if (c < 0 || c >= m.c)
         return 0;
     return get_pixel(m, x, y, c);
@@ -277,24 +246,22 @@ void letterbox_image_into(image im, int w, int h, image boxed)
 
 void Detector::fetch_in_thread()
 {
-    free_image(buff[buff_index]);
-    buff[buff_index] = get_image_from_stream(cap);
-    if (buff[buff_index].data == 0)
+    free_image(buff);
+    buff = get_image_from_stream(cap);
+    if (buff.data == 0)
     {
-        demo_done = 1;
         return;
     }
-    buff_letter[buff_index] = letterbox_image(buff[buff_index], net->w, net->h);
-    // letterbox_image_into(buff[buff_index], net->w, net->h, );
+    // buff_letter[buff_index] = letterbox_image(buff[buff_index], net->w, net->h);
+    letterbox_image_into(buff, net->w, net->h, buff_letter);
 }
 
 void Detector::detect_in_thread()
 {
-    bool running = true;
     float nms = .4;
 
     layer l = net->layers[net->n - 1];
-    float *X = buff_letter[(buff_index + 2) % 3].data;
+    float *X = buff_letter.data;
     network_predict(net, X);
 
     remember_network(net);
@@ -309,20 +276,19 @@ void Detector::detect_in_thread()
     printf("\033[1;1H");
     printf("\nFPS:%.1f\n", fps);
     printf("Objects:\n\n");
-    image display = buff[(buff_index + 2) % 3];
+    image display = buff;
     draw_detections(
         display, dets, nboxes, demo_thresh, demo_names, demo_alphabet, demo_classes);
     free_detections(dets, nboxes);
 
-    demo_index = (demo_index + 1) % demo_frame;
-    running = false;
+    avg_index = (avg_index + 1) % avg_frame;
 }
 
 int main(int argc, char *argv[])
 {
     auto datacfg = argv[1];
     list *options = read_data_cfg(datacfg);
-    char *name_list = option_find_str(options, "names", "data/names.list");
+    char *name_list = option_find_str(options, (char *)"names", (char *)"data/names.list");
 
     auto cfg = argv[2];
     auto weights = argv[3];
@@ -330,7 +296,7 @@ int main(int argc, char *argv[])
     auto cam_index = 1;
     auto filename = nullptr;
     auto **names = get_labels(name_list);
-    auto classes = option_find_int(options, "classes", 20);
+    auto classes = option_find_int(options, (char *)"classes", 20);
     auto frame_skip = 0;
     auto prefix = nullptr;
     auto avg = 3;
