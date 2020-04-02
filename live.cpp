cada170229f223b1cc58706051b04db9d435dfa5
Paul Lesur <git@lesurpaul.fr>
2020-04-02 22:04:24 +0200

    alomost done!

--- live.cpp
+++ live.cpp
@@ -7,39 +7,24 @@
 #include "darknet.h"
 #include <fmt/printf.h>
 #include <opencv2/core.hpp>
-#include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 #include <opencv2/videoio.hpp>
 
-void draw_detections(cv::Mat &f,
-                     detection *dets,
-                     int num,
-                     float thresh,
-                     char **names,
-                     image **alphabet,
-                     int classes)
+void draw_detections(
+    cv::Mat &f, detection *dets, int num, float thresh, char **names, int classes)
 {
-    int i, j;
-
-    for (i = 0; i < num; ++i)
+    for (int i = 0; i < num; ++i)
     {
-        char labelstr[4096] = {0};
         int class_ = -1;
-        for (j = 0; j < classes; ++j)
+        for (int j = 0; j < classes; ++j)
         {
             if (dets[i].prob[j] > thresh)
             {
-                if (class_ < 0)
-                {
-                    strcat(labelstr, names[j]);
-                    class_ = j;
-                }
-                else
-                {
-                    strcat(labelstr, ", ");
-                    strcat(labelstr, names[j]);
-                }
-                printf("%s: %.0f%%\n", names[j], dets[i].prob[j] * 100);
+                // note that we could have other values above the threshold, but the
+                // classes are ordered in decreasing order
+                class_ = j;
+                break;
             }
         }
         if (class_ < 0)
@@ -47,18 +32,10 @@ void draw_detections(cv::Mat &f,
             continue;
         }
 
-        float red = 1.0;
-        float green = 1.0;
+        float red = 255.0;
+        float green = 255.0;
         float blue = 0.0;
-        float rgb[3];
-
-        // width = prob*20+2;
-
-        rgb[0] = red;
-        rgb[1] = green;
-        rgb[2] = blue;
         box b = dets[i].bbox;
-        // printf("%f %f %f %f\n", b.x, b.y, b.w, b.h);
 
         int left = (b.x - b.w / 2.) * f.cols;
         int right = (b.x + b.w / 2.) * f.cols;
@@ -85,6 +62,7 @@ void draw_detections(cv::Mat &f,
                     0,
                     1.0,
                     cv::Scalar_(red, green, blue));
+        // TODO: what is this..?
         /*
         if (dets[i].mask)
         {
@@ -163,69 +141,96 @@ int size_network(network *net)
     return count;
 }
 
-struct Detector
+class Detector
 {
-    float detection_threshold() const { return detection_threshold_; }
-    char **labels() const { return labels_; }
-
-    char **labels_;
-
-    network *net;
-    image buff;
-    image buff_letter;
-    float detection_threshold_ = 0;
-    float demo_hier = .5;
-    int running = 0;
-
-    int avg_frame = 3;
-    int avg_index = 0;
-    float **predictions;
-    float *avg;
-    int demo_total = 0;
-
-    std::pair<detection *, int> detect_in_thread();
-    void remember_network(network *net);
-    detection *avg_predictions(network *net, int *nboxes);
-
-    Detector(char *cfgfile,
+  public:
+    Detector(char *datacfgfile,
+             char *cfgfile,
              char *weightfile,
              float thresh,
-             char **names,
              int /* delay */,
              int avg_frames,
              float hier,
              cv::Mat const &init_f)
+        : net(load_network(cfgfile, weightfile, 0)), detection_threshold_(thresh),
+          hier_threshold_(hier), avg_frames_(avg_frames),
+          classes_(net->layers[net->n - 1].classes), number_layers_(size_network(net))
     {
-        avg_frame = avg_frames;
-        labels_ = names;
-        detection_threshold_ = thresh;
-        demo_hier = hier;
-        printf("Demo\n");
-        net = load_network(cfgfile, weightfile, 0);
+        list *options = read_data_cfg(datacfgfile);
+        char *name_list =
+            option_find_str(options, (char *)"names", (char *)"data/names.list");
+
+        labels_ = get_labels(name_list);
         set_batch_network(net, 1);
 
-        int i;
-
-        demo_total = size_network(net);
-        predictions = reinterpret_cast<float **>(calloc(avg_frame, sizeof(float *)));
-        for (i = 0; i < avg_frame; ++i)
+        predictions = reinterpret_cast<float **>(calloc(avg_frames_, sizeof(float *)));
+        for (int i = 0; i < avg_frames_; ++i)
         {
-            predictions[i] = reinterpret_cast<float *>(calloc(demo_total, sizeof(float)));
+            predictions[i] =
+                reinterpret_cast<float *>(calloc(number_layers_, sizeof(float)));
         }
-        avg = reinterpret_cast<float *>(calloc(demo_total, sizeof(float)));
+        avg = reinterpret_cast<float *>(calloc(number_layers_, sizeof(float)));
 
         buff = mat_to_image(init_f);
         buff_letter = letterbox_image(buff, net->w, net->h);
     }
 
-    std::pair<detection *, int> run_once(cv::Mat const &image)
+    float detection_threshold() const { return detection_threshold_; }
+    float &detection_threshold() { return detection_threshold_; }
+
+    float hier_threshold() const { return hier_threshold_; }
+    float &hier_threshold() { return hier_threshold_; }
+
+    char **labels() const { return labels_; }
+    int classes() const { return classes_; }
+
+    std::pair<detection *, int> run(cv::Mat const &image)
     {
         free_image(buff);
         buff = mat_to_image(image);
         assert(buff.data != 0);
         letterbox_image_into(buff, net->w, net->h, buff_letter);
-        return detect_in_thread();
+
+        float nms = .4;
+
+        layer l = net->layers[net->n - 1];
+        float *X = buff_letter.data;
+        network_predict(net, X);
+
+        remember_network(net);
+        detection *dets = 0;
+        int nboxes = 0;
+        dets = avg_predictions(net, &nboxes);
+
+        if (nms > 0)
+            do_nms_obj(dets, nboxes, l.classes, nms);
+
+        avg_index = (avg_index + 1) % avg_frames_;
+        return {dets, nboxes};
     }
+
+  private:
+    network *net;
+    float detection_threshold_;
+    float hier_threshold_;
+    int avg_frames_;
+    int classes_;
+    int number_layers_;
+
+    char **labels_;
+
+    image buff;
+    image buff_letter;
+
+    int avg_index = 0;
+    float **predictions;
+    float *avg;
+    // TODO: not sure about the name (wth is this?)
+
+    void remember_network(network *net);
+    detection *avg_predictions(network *net, int *nboxes);
+
+    // TODO dtor
 
     static image mat_to_image(cv::Mat const &src)
     {
@@ -274,10 +279,10 @@ detection *Detector::avg_predictions(network *net, int *nboxes)
 {
     int i, j;
     int count = 0;
-    fill_cpu(demo_total, 0, avg, 1);
-    for (j = 0; j < avg_frame; ++j)
+    fill_cpu(number_layers_, 0, avg, 1);
+    for (j = 0; j < avg_frames_; ++j)
     {
-        axpy_cpu(demo_total, 1. / avg_frame, predictions[j], 1, avg, 1);
+        axpy_cpu(number_layers_, 1. / avg_frames_, predictions[j], 1, avg, 1);
     }
     for (i = 0; i < net->n; ++i)
     {
@@ -289,48 +294,27 @@ detection *Detector::avg_predictions(network *net, int *nboxes)
         }
     }
     detection *dets = get_network_boxes(
-        net, buff.w, buff.h, detection_threshold_, demo_hier, 0, 1, nboxes);
+        net, buff.w, buff.h, detection_threshold_, hier_threshold_, 0, 1, nboxes);
     return dets;
-}
-
-std::pair<detection *, int> Detector::detect_in_thread()
-{
-    float nms = .4;
-
-    layer l = net->layers[net->n - 1];
-    float *X = buff_letter.data;
-    network_predict(net, X);
-
-    remember_network(net);
-    detection *dets = 0;
-    int nboxes = 0;
-    dets = avg_predictions(net, &nboxes);
-
-    if (nms > 0)
-        do_nms_obj(dets, nboxes, l.classes, nms);
-
-    avg_index = (avg_index + 1) % avg_frame;
-    return {dets, nboxes};
 }
 
 int main(int argc, char *argv[])
 {
-    auto datacfg = argv[1];
-    list *options = read_data_cfg(datacfg);
-    char *name_list =
-        option_find_str(options, (char *)"names", (char *)"data/names.list");
+    if (argc < 5)
+    {
+        fmt::print("Not enough arguments, 5 required");
+        return 1;
+    }
 
+    auto datacfg = argv[1];
     auto cfg = argv[2];
     auto weights = argv[3];
     auto cam = argv[4];
     auto thresh = 0.5;
-    auto **names = get_labels(name_list);
-    auto classes = option_find_int(options, (char *)"classes", 20);
+    // auto classes = option_find_int(options, (char *)"classes", 20);
     auto frame_skip = 0;
     auto avg = 3;
     auto hier_thresh = 0.5;
-
-    image **alphabet = load_alphabet();
 
     cv::VideoCapture cap(cam);
     if (!cap.isOpened())
@@ -341,50 +325,46 @@ int main(int argc, char *argv[])
 
     cv::Mat f;
     cap >> f;
-    auto d = Detector(cfg, weights, thresh, names, frame_skip, avg, hier_thresh, f);
+    auto d = Detector(datacfg, cfg, weights, thresh, frame_skip, avg, hier_thresh, f);
 
     make_window((char *)"Demo", 1352, 1013, 0);
     auto start = what_time_is_it_now();
     for (;;)
     {
         cap >> f;
-        auto [dets, nboxes] = d.run_once(f);
+        auto [dets, nboxes] = d.run(f);
 
         auto end = what_time_is_it_now();
         auto fps = 1. / (end - start);
         start = end;
-        printf("\nFPS:%.1f\n", fps);
-        printf("Objects:\n\n");
+        fmt::printf("\nFPS:%.1f\n", fps);
+        fmt::printf("Objects:\n\n");
         draw_detections(
-            f, dets, nboxes, d.detection_threshold(), d.labels(), alphabet, classes);
+            f, dets, nboxes, d.detection_threshold(), d.labels(), d.classes());
+        // TODO: make some wrapper around this (with raii)
         free_detections(dets, nboxes);
         cv::imshow("asd", f);
-        cv::waitKey(1);
-        //int c = show_image(d.buff, "Demo", 1);
-        //if (c != -1)
-            //c = c % 256;
-        /*
-        if (c == 82)
+        char c = cv::waitKey(1);
+        switch (c)
         {
-            demo_thresh += .02;
+        case 'q':
+            return 0;
+        case 'j':
+            d.detection_threshold() -= 0.02;
+            d.detection_threshold() = std::max(0.02f, d.detection_threshold());
+            break;
+        case 'k':
+            d.detection_threshold() += 0.02;
+            break;
+            // TODO: what is this for?
+        case 'h':
+            d.hier_threshold() -= 0.02;
+            d.detection_threshold() = std::max(0.0f, d.detection_threshold());
+            break;
+        case 'l':
+            d.hier_threshold() += 0.02;
+            break;
         }
-        else if (c == 84)
-        {
-            demo_thresh -= .02;
-            if (demo_thresh <= .02)
-                demo_thresh = .02;
-        }
-        else if (c == 83)
-        {
-            demo_hier += .02;
-        }
-        else if (c == 81)
-        {
-            demo_hier -= .02;
-            if (demo_hier <= .0)
-                demo_hier = .0;
-        }
-        */
     }
 
     return 0;
