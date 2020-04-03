755de37e349da07d796e9fe044793f2ca2100cf2
Paul Lesur <git@lesurpaul.fr>
2020-04-03 16:33:38 +0200

    cleanup underlying types

--- include/darknet/detector.hpp
+++ include/darknet/detector.hpp
@@ -1,85 +1,21 @@
 #ifndef DETECTOR_HPP_YDSL4IM7
 #define DETECTOR_HPP_YDSL4IM7
 
-#include "assert.h"
-#include <opencv2/core.hpp>
-#include <opencv2/imgproc.hpp>
-
-// TODO split this in a cpp file so we can avoid including darknet. here
-// this would allow us to contain those macro definitions
+// NOTE: I had some very crtp based solution to avoid including this,
+// but client code needs to access the detection objects
+// We could just "manually" wrap this, but this sucks
+// The cleanest method would be to patch darknet, but then all the crtp solutions would be
+// unneeded :)
 #define OPENCV
 #define GPU
 #define CUDNN
 #include "darknet.h"
-
-// TODO: get rid of asserts when with have contracts (or whatever is replacing them)
+#include <memory>
+#include <opencv2/core.hpp>
+#include <vector>
 
 namespace darknet
 {
-
-static float get_pixel(image m, int x, int y, int c)
-{
-    assert(x < m.w && y < m.h && c < m.c);
-    return m.data[c * m.h * m.w + y * m.w + x];
-}
-static void set_pixel(image m, int x, int y, int c, float val)
-{
-    if (x < 0 || y < 0 || c < 0 || x >= m.w || y >= m.h || c >= m.c)
-        return;
-    assert(x < m.w && y < m.h && c < m.c);
-    m.data[c * m.h * m.w + y * m.w + x] = val;
-}
-
-void embed_image(image source, image dest, int dx, int dy)
-{
-    int x, y, k;
-    for (k = 0; k < source.c; ++k)
-    {
-        for (y = 0; y < source.h; ++y)
-        {
-            for (x = 0; x < source.w; ++x)
-            {
-                float val = get_pixel(source, x, y, k);
-                set_pixel(dest, dx + x, dy + y, k, val);
-            }
-        }
-    }
-}
-
-void letterbox_image_into(image im, int w, int h, image boxed)
-{
-    int new_w = im.w;
-    int new_h = im.h;
-    if (((float)w / im.w) < ((float)h / im.h))
-    {
-        new_w = w;
-        new_h = (im.h * w) / im.w;
-    }
-    else
-    {
-        new_h = h;
-        new_w = (im.w * h) / im.h;
-    }
-    image resized = resize_image(im, new_w, new_h);
-    embed_image(resized, boxed, (w - new_w) / 2, (h - new_h) / 2);
-    free_image(resized);
-}
-
-int size_network(network *net)
-{
-    int i;
-    int count = 0;
-    for (i = 0; i < net->n; ++i)
-    {
-        layer l = net->layers[i];
-        if (l.type == YOLO || l.type == REGION || l.type == DETECTION)
-        {
-            count += l.outputs;
-        }
-    }
-    return count;
-}
-
 class Detections
 {
   public:
@@ -100,36 +36,23 @@ class Detections
 class Detector
 {
   public:
-    Detector(char *datacfgfile,
-             char *cfgfile,
+    Detector(char *cfgfile,
              char *weightfile,
+             char *labelfile,
              float thresh,
-             int /* delay */,
+             int delay,
              int avg_frames,
              float hier,
-             cv::Mat const &init_f)
-        : net(load_network(cfgfile, weightfile, 0)), detection_threshold_(thresh),
-          hier_threshold_(hier), avg_frames_(avg_frames),
-          classes_(net->layers[net->n - 1].classes), number_layers_(size_network(net))
-    {
-        list *options = read_data_cfg(datacfgfile);
-        char *name_list =
-            option_find_str(options, (char *)"names", (char *)"data/names.list");
+             cv::Mat const &init_f);
 
-        labels_ = get_labels(name_list);
-        set_batch_network(net, 1);
-
-        predictions = reinterpret_cast<float **>(calloc(avg_frames_, sizeof(float *)));
-        for (int i = 0; i < avg_frames_; ++i)
-        {
-            predictions[i] =
-                reinterpret_cast<float *>(calloc(number_layers_, sizeof(float)));
-        }
-        avg = reinterpret_cast<float *>(calloc(number_layers_, sizeof(float)));
-
-        buff = mat_to_image(init_f);
-        buff_letter = letterbox_image(buff, net->w, net->h);
-    }
+    static Detector from_data_cfg(char *cfgfile,
+                                  char *weightfile,
+                                  char *datacfg,
+                                  float thresh,
+                                  int delay,
+                                  int avg_frames,
+                                  float hier,
+                                  cv::Mat const &init_f);
 
     float detection_threshold() const { return detection_threshold_; }
     float &detection_threshold() { return detection_threshold_; }
@@ -137,189 +60,43 @@ class Detector
     float hier_threshold() const { return hier_threshold_; }
     float &hier_threshold() { return hier_threshold_; }
 
-    char **labels() const { return labels_; }
+    std::vector<std::string> labels() const { return labels_; }
     int classes() const { return classes_; }
 
-    Detections run(cv::Mat const &image)
-    {
-        free_image(buff);
-        buff = mat_to_image(image);
-        assert(buff.data != 0);
-        letterbox_image_into(buff, net->w, net->h, buff_letter);
-
-        float nms = .4;
-
-        layer l = net->layers[net->n - 1];
-        float *X = buff_letter.data;
-        network_predict(net, X);
-
-        remember_network(net);
-        detection *dets = 0;
-        int nboxes = 0;
-        dets = avg_predictions(net, &nboxes);
-
-        if (nms > 0)
-            do_nms_obj(dets, nboxes, l.classes, nms);
-
-        avg_index = (avg_index + 1) % avg_frames_;
-        return Detections{dets, nboxes};
-    }
+    Detections run(cv::Mat const &image);
 
   private:
-    network *net;
+    // This is need because 'image' is declared as an anonymous struct.
+    // This means we *cannot* forward decalre it, and thence use a proxy struct
+    // NOTE: this *might* actually be a better design,
+    // as we don't leak any forward declaration this way
+    // whereas we leak the 'network' types etc. early in this file
+    struct Image;
+    static void free_image_wrap(Image *i);
+
+    template <auto fn> using Deleter = std::integral_constant<decltype(fn), fn>;
+    std::unique_ptr<network, Deleter<free_network>> net;
+
     float detection_threshold_;
+    // TODO: not sure about the name (wth is this?)
     float hier_threshold_;
     int avg_frames_;
     int classes_;
     int number_layers_;
 
-    char **labels_;
+    std::vector<std::string> labels_;
 
-    image buff;
-    image buff_letter;
+    // NOTE: this needs to be deleted manually (well, its content)
+    std::unique_ptr<Image, Deleter<free_image_wrap>> buff_;
+    std::unique_ptr<Image, Deleter<free_image_wrap>> buff_letter_;
 
     int avg_index = 0;
-    float **predictions;
-    float *avg;
-    // TODO: not sure about the name (wth is this?)
+    std::vector<std::unique_ptr<float[]>> predictions_;
+    std::unique_ptr<float[]> avg_;
 
     void remember_network(network *net);
     detection *avg_predictions(network *net, int *nboxes);
-
-    // TODO dtor
-
-    static image mat_to_image(cv::Mat const &src)
-    {
-        int h = src.rows;
-        int w = src.cols;
-        int c = src.channels();
-        image im = make_image(w, h, c);
-        unsigned char *data = (unsigned char *)src.data;
-        int step = w * c;
-
-        for (int i = 0; i < h; ++i)
-        {
-            for (int j = 0; j < w; ++j)
-            {
-                for (int k = 0; k < c; ++k)
-                {
-                    // NOTE: we do c - k - 1, while the original code does just k,
-                    // but swap the two channels manually afterwards
-                    im.data[k * w * h + i * w + j] =
-                        data[i * step + j * c + (c - k - 1)] / 255.;
-                }
-            }
-        }
-        return im;
-    }
 };
-
-void Detector::remember_network(network *net)
-{
-    int i;
-    int count = 0;
-    for (i = 0; i < net->n; ++i)
-    {
-        layer l = net->layers[i];
-        if (l.type == YOLO || l.type == REGION || l.type == DETECTION)
-        {
-            memcpy(predictions[avg_index] + count,
-                   net->layers[i].output,
-                   sizeof(float) * l.outputs);
-            count += l.outputs;
-        }
-    }
-}
-
-detection *Detector::avg_predictions(network *net, int *nboxes)
-{
-    int i, j;
-    int count = 0;
-    fill_cpu(number_layers_, 0, avg, 1);
-    for (j = 0; j < avg_frames_; ++j)
-    {
-        axpy_cpu(number_layers_, 1. / avg_frames_, predictions[j], 1, avg, 1);
-    }
-    for (i = 0; i < net->n; ++i)
-    {
-        layer l = net->layers[i];
-        if (l.type == YOLO || l.type == REGION || l.type == DETECTION)
-        {
-            memcpy(l.output, avg + count, sizeof(float) * l.outputs);
-            count += l.outputs;
-        }
-    }
-    detection *dets = get_network_boxes(
-        net, buff.w, buff.h, detection_threshold_, hier_threshold_, 0, 1, nboxes);
-    return dets;
-}
-
-void draw_detections(
-    cv::Mat &f, Detections const &detections, float thresh, char **names, int classes)
-{
-    for (auto det : detections)
-    {
-        int class_ = -1;
-        for (int j = 0; j < classes; ++j)
-        {
-            if (det.prob[j] > thresh)
-            {
-                // note that we could have other values above the threshold, but the
-                // classes are ordered in decreasing order
-                class_ = j;
-                break;
-            }
-        }
-        if (class_ < 0)
-        {
-            continue;
-        }
-
-        float red = 255.0;
-        float green = 255.0;
-        float blue = 0.0;
-        box b = det.bbox;
-
-        int left = (b.x - b.w / 2.) * f.cols;
-        int right = (b.x + b.w / 2.) * f.cols;
-        int top = (b.y - b.h / 2.) * f.rows;
-        int bot = (b.y + b.h / 2.) * f.rows;
-
-        if (left < 0)
-            left = 0;
-        if (right > f.cols - 1)
-            right = f.cols - 1;
-        if (top < 0)
-            top = 0;
-        if (bot > f.rows - 1)
-            bot = f.rows - 1;
-
-        cv::rectangle(f,
-                      cv::Point(left, top),
-                      cv::Point(right, bot),
-                      cv::Scalar(red, green, blue),
-                      1);
-        cv::putText(f,
-                    names[class_],
-                    cv::Point(left, bot),
-                    0,
-                    1.0,
-                    cv::Scalar_(red, green, blue));
-        // TODO: what is this..?
-        /*
-        if (det.mask)
-        {
-            image mask = float_to_image(14, 14, 1, dets[i].mask);
-            image resized_mask = resize_image(mask, b.w * f.cols, b.h * f.rows);
-            image tmask = threshold_image(resized_mask, .5);
-            embed_image(tmask, im, left, top);
-            free_image(mask);
-            free_image(resized_mask);
-            free_image(tmask);
-        }
-        */
-    }
-}
 } // namespace darknet
 
 #endif /* end of include guard: DETECTOR_HPP_YDSL4IM7 */
--- src/detector.cpp
+++ src/detector.cpp
@@ -1,1 +1,227 @@
 #include "darknet/detector.hpp"
+#include <fstream>
+
+// TODO: get rid of asserts when with have contracts (or whatever is replacing them)
+
+namespace
+{
+float get_pixel(image m, int x, int y, int c)
+{
+    assert(x < m.w && y < m.h && c < m.c);
+    return m.data[c * m.h * m.w + y * m.w + x];
+}
+void set_pixel(image m, int x, int y, int c, float val)
+{
+    if (x < 0 || y < 0 || c < 0 || x >= m.w || y >= m.h || c >= m.c)
+        return;
+    assert(x < m.w && y < m.h && c < m.c);
+    m.data[c * m.h * m.w + y * m.w + x] = val;
+}
+
+void embed_image(image source, image dest, int dx, int dy)
+{
+    int x, y, k;
+    for (k = 0; k < source.c; ++k)
+    {
+        for (y = 0; y < source.h; ++y)
+        {
+            for (x = 0; x < source.w; ++x)
+            {
+                float val = get_pixel(source, x, y, k);
+                set_pixel(dest, dx + x, dy + y, k, val);
+            }
+        }
+    }
+}
+
+void letterbox_image_into(image im, int w, int h, image boxed)
+{
+    int new_w = im.w;
+    int new_h = im.h;
+    if (((float)w / im.w) < ((float)h / im.h))
+    {
+        new_w = w;
+        new_h = (im.h * w) / im.w;
+    }
+    else
+    {
+        new_h = h;
+        new_w = (im.w * h) / im.h;
+    }
+    image resized = resize_image(im, new_w, new_h);
+    embed_image(resized, boxed, (w - new_w) / 2, (h - new_h) / 2);
+    free_image(resized);
+}
+
+int size_network(network *net)
+{
+    int i;
+    int count = 0;
+    for (i = 0; i < net->n; ++i)
+    {
+        layer l = net->layers[i];
+        if (l.type == YOLO || l.type == REGION || l.type == DETECTION)
+        {
+            count += l.outputs;
+        }
+    }
+    return count;
+}
+image mat_to_image(cv::Mat const &src)
+{
+    int h = src.rows;
+    int w = src.cols;
+    int c = src.channels();
+    image im = make_image(w, h, c);
+    unsigned char *data = (unsigned char *)src.data;
+    int step = w * c;
+
+    for (int i = 0; i < h; ++i)
+    {
+        for (int j = 0; j < w; ++j)
+        {
+            for (int k = 0; k < c; ++k)
+            {
+                // NOTE: we do c - k - 1, while the original code does just k,
+                // but swap the two channels manually afterwards
+                im.data[k * w * h + i * w + j] =
+                    data[i * step + j * c + (c - k - 1)] / 255.;
+            }
+        }
+    }
+    return im;
+}
+std::vector<std::string> load_labels(std::string const &labels_path)
+{
+    std::ifstream f(labels_path);
+    std::vector<std::string> labels;
+    for (std::string label; std::getline(f, label);)
+    {
+        labels.emplace_back(std::move(label));
+    }
+    labels.shrink_to_fit();
+    return labels;
+}
+
+} // namespace
+
+namespace darknet
+{
+
+struct Detector::Image : public image
+{
+    Image(image &&i) : image(i) {}
+    Image &operator=(image &&i)
+    {
+        free_image(*this);
+        new (this) Image(std::move(i));
+        return *this;
+    }
+};
+void Detector::free_image_wrap(Image *i) { free_image(*static_cast<image *>(i)); }
+
+Detector::Detector(char *cfgfile,
+                   char *weightfile,
+                   char *labelfile,
+                   float thresh,
+                   int /* delay */,
+                   int avg_frames,
+                   float hier,
+                   cv::Mat const &init_f)
+    : net(load_network(cfgfile, weightfile, 0)), detection_threshold_(thresh),
+      hier_threshold_(hier), avg_frames_(avg_frames),
+      classes_(net->layers[net->n - 1].classes), number_layers_(size_network(net.get())),
+      labels_(load_labels(labelfile)), buff_(new Image(mat_to_image(init_f))),
+      buff_letter_(new Image(letterbox_image(*buff_, net->w, net->h))),
+      avg_(std::make_unique<float[]>(number_layers_))
+{
+    set_batch_network(net.get(), 1);
+
+    predictions_.resize(avg_frames_);
+    for (auto &pred : predictions_)
+    {
+        pred = std::make_unique<float[]>(number_layers_);
+    }
+}
+
+Detector Detector::from_data_cfg(char *cfgfile,
+                                 char *weightfile,
+                                 char *datacfg,
+                                 float thresh,
+                                 int delay,
+                                 int avg_frames,
+                                 float hier,
+                                 cv::Mat const &init_f)
+{
+    list *options = read_data_cfg(datacfg);
+    char *labelfile =
+        option_find_str(options, (char *)"names", (char *)"data/names.list");
+    return Detector(
+        cfgfile, weightfile, labelfile, thresh, delay, avg_frames, hier, init_f);
+}
+
+Detections Detector::run(cv::Mat const &image)
+{
+    *buff_ = mat_to_image(image);
+    assert(buff_->data != 0);
+    letterbox_image_into(*buff_, net->w, net->h, *buff_letter_);
+
+    float nms = .4;
+
+    layer l = net->layers[net->n - 1];
+    float *X = buff_letter_->data;
+    network_predict(net.get(), X);
+
+    remember_network(net.get());
+    detection *dets = 0;
+    int nboxes = 0;
+    dets = avg_predictions(net.get(), &nboxes);
+
+    if (nms > 0)
+        do_nms_obj(dets, nboxes, l.classes, nms);
+
+    avg_index = (avg_index + 1) % avg_frames_;
+    return Detections{dets, nboxes};
+}
+
+void Detector::remember_network(network *net)
+{
+    int i;
+    int count = 0;
+    for (i = 0; i < net->n; ++i)
+    {
+        layer l = net->layers[i];
+        if (l.type == YOLO || l.type == REGION || l.type == DETECTION)
+        {
+            memcpy(predictions_[avg_index].get() + count,
+                   net->layers[i].output,
+                   sizeof(float) * l.outputs);
+            count += l.outputs;
+        }
+    }
+}
+
+detection *Detector::avg_predictions(network *net, int *nboxes)
+{
+    int i, j;
+    int count = 0;
+    fill_cpu(number_layers_, 0, avg_.get(), 1);
+    for (j = 0; j < avg_frames_; ++j)
+    {
+        axpy_cpu(
+            number_layers_, 1. / avg_frames_, predictions_[j].get(), 1, avg_.get(), 1);
+    }
+    for (i = 0; i < net->n; ++i)
+    {
+        layer l = net->layers[i];
+        if (l.type == YOLO || l.type == REGION || l.type == DETECTION)
+        {
+            memcpy(l.output, avg_.get() + count, sizeof(float) * l.outputs);
+            count += l.outputs;
+        }
+    }
+    detection *dets = get_network_boxes(
+        net, buff_->w, buff_->h, detection_threshold_, hier_threshold_, 0, 1, nboxes);
+    return dets;
+}
+} // namespace darknet
